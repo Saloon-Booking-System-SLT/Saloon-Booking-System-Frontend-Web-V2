dc04cf5cf69a8d3254d8997705c295e530b016f0
ishani2924 <167461216+ishani2924@users.noreply.github.com>
2025-12-04 08:27:03 +0530

    commit 1

--- src/Components/Customer/SelectProfessionalPage.js
+++ src/Components/Customer/SelectProfessionalPage.js
@@ -3,10 +3,6 @@ import { useLocation, useNavigate } from "react-router-dom";
 import "./SelectServicesPage.css";
 
 
-const API_BASE_URL = process.env.REACT_APP_API_URL
-  ? process.env.REACT_APP_API_URL.replace("/api", "")
-  : "http://localhost:5000";
-
 const API_BASE_URL = process.env.REACT_APP_API_URL ? 
   process.env.REACT_APP_API_URL.replace('/api', '') : 
   'https://saloon-booking-system-backend-v2.onrender.com';
